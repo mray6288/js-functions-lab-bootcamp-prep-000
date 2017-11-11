83b82185249c71f010387834446fe0e3741f5975
Ray Blasko <mray6288@gmail.com>
2017-11-11 05:30:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,10 @@ function happyHolidaysTo(name){
   return 'Happy holidays, ${name}!'
 }
 
+function happyHolidayTo(holiday, name){
+  return 'Happy ${holiday}, ${name}!'
+}
+
 console.log(happyHolidays())
 console.log(happyHolidaysTo('Ray'))
+console.log(happyHolidayTo('Halloween','Ray'))
